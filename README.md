48df22221ee553aa89d2f21c90bd18ce5ff6ed5e
neikvon <neikvon@hotmail.com>
2018-07-06 01:24:09 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # fbi-task-pack
 Pack files or folders.
+
+> This is a fbi task. If you haven't installed [fbi](https://github.com/AlloyTeam/fbi) yet, use the following command to install.
+>
+> `$ npm i -g fbi` or `yarn global add fbi`
+
+## Requirements
+- `fbi v3.0+`
+- `node v7.6+`
+
+## Features
+
+- pack files/folders with [archiver](https://github.com/archiverjs/node-archiver)
+
+## Usage
+
+**Install**
+
+```bash
+$ fbi add https://github.com/fbi-templates/fbi-task-pack.git
+```
+
+**Run**
+
+```bash
+$ cd path/to/any/directory
+$ fbi pack
+```
+
+## Params
+
+- `-i/-input`: Pattern to be matched. [docs](https://github.com/isaacs/node-glob#glob-primer)
+  - example: `$ fbi pack -i=example/sub-folder`
+- `-ignore`: A pattern or an array of glob patterns to exclude matches
+  - defaults: `.DS_Store,*/.DS_Store,**/*/.DS_Store,**/.git/**,**/.svn/**`
+  - example: `$ fbi pack -ignore=**/x/**,a.txt`
+
+## More
+- [Official templates](https://github.com/fbi-templates)
+- [`fbi` documentation](https://neikvon.gitbooks.io/fbi/content/)
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
+
+## Changelog
+
+- **1.0.0** (2018.07.06)
+  - init
--- index.js
+++ index.js
@@ -1,3 +1,120 @@
-function pack() {}
+const fs = require('fs')
+const path = require('path')
+const archiver = require('archiver')
+
+async function pack () {
+  const root = process.cwd()
+  const params = ctx.task.getParams('pack')
+  const _input = params.i || params.input || ''
+
+  // -ignore=**/x/**
+  let ignore = params.ignore ? params.ignore.split(',') : []
+  ignore = Array.from(
+    new Set(
+      ignore.concat([
+        '.DS_Store',
+        '*/.DS_Store',
+        '**/*/.DS_Store',
+        '**/.git/**',
+        '**/.svn/**'
+      ])
+    )
+  )
+
+  const input = ctx.utils.path.isAbsolute(_input)
+    ? _input
+    : path.join(root, _input)
+
+  ctx.logger.log('Input:', input)
+
+  const inputArr = input.split('/')
+  inputArr.reduceRight((prev, curr, index, array) => {
+    if (!prev || prev.startsWith('*')) {
+      array.pop()
+    }
+    return curr
+  })
+  const inputPath = inputArr.join('/')
+
+  if (!await ctx.utils.fs.exist(inputPath)) {
+    ctx.logger.error(`Input not found.`)
+    process.exit(1)
+  }
+
+  const inputStat = await ctx.utils.fs.stat(inputPath)
+  const isFile = inputStat.isFile()
+  const globRoot = isFile ? path.dirname(inputPath) : inputPath
+
+  const outputName = path.basename(inputArr.join('/'))
+  const output = path.join(
+    input === root ? path.dirname(root) : root,
+    `${outputName}.zip`
+  )
+  ctx.logger.log('output:', output)
+
+  let globPattern = input.replace(globRoot, '')
+  if (globPattern.startsWith('/')) {
+    globPattern = globPattern.replace('/', '')
+  }
+  globPattern = globPattern || '**/**'
+
+  return new Promise((resolve, reject) => {
+    const ws = fs.createWriteStream(output)
+    const archive = archiver('zip', {
+      zlib: {
+        level: 9
+      }
+    })
+
+    ws.on('close', function () {
+      let unit = 'bytes'
+      let size = archive.pointer()
+      if (size > 1000) {
+        size = size / 1024
+        unit = 'KB'
+      }
+
+      if (size > 1000) {
+        size = size / 1024
+        unit = 'MB'
+      }
+
+      if (size > 1000) {
+        size = size / 1024
+        unit = 'GB'
+      }
+
+      ctx.logger.success(
+        `Pack success. File Size: ${size.toFixed(2)} ${unit}`
+      )
+      resolve()
+    })
+
+    ws.on('end', function () {
+      console.log('Data has been drained')
+    })
+
+    archive.on('warning', function (err) {
+      reject(err)
+    })
+
+    archive.on('error', function (err) {
+      reject(err)
+    })
+
+    archive.pipe(ws)
+
+    archive.glob(globPattern, {
+      cwd: globRoot,
+      nomount: true,
+      dot: true,
+      debug: !!ctx.mode.debug,
+      absolute: false,
+      ignore: ignore
+    })
+
+    archive.finalize()
+  })
+}
 
 module.exports = pack
